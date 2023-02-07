e2e5d7f1b365225d2c71518f7e0abd3083f3248b
Luciano Castro <lfcastro2911@gmail.com>
2023-02-07 17:42:19 -0300

    modified with Generics execute function to get type Decodable

--- Rick and Morty LC/ApiClient/RMRequest.swift
+++ Rick and Morty LC/ApiClient/RMRequest.swift
@@ -10,4 +10,50 @@ import Foundation
 /// Object that represent a single API call
 final class RMRequest{
     
+    private struct Constants{
+        static let baseURL = "https://rickandmortyapi.com/api"
+    }
+    
+    private let endpoint: RMEndpoint
+    
+    private let pathComponents : [String]
+    
+    private let queryParameters: [URLQueryItem]
+    
+    /// Constructed url for the api request in string format
+    private var urlString: String {
+        var string = Constants.baseURL
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        if !queryParameters.isEmpty {
+            string += "?"
+            // We transform with compactMap to an array with the query formula name=value plus & with joined()
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else{ return nil} //Unwraped that value
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        
+        return string
+    }
+    
+    private var url: URL?{
+        return URL(string: urlString)
+    }
+    // MARK: Public
+    
+    init(endpoint: RMEndpoint, pathComponents: [String], queryParameters: [URLQueryItem]) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
